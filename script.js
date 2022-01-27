d833622ae7f21374152a0753661b4b5210215773
flyether <flyether@gmail.com>
2022-01-27 18:31:40 +0300

    ff

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
 document.body.innerHTML = '<h1> Hello from JS</h1>'
 console.log('horse')
+
+const student = {
+   email: 'Valera',
+   name: 'tld@tut/by',
+   score:{
+      task1: 90,
+      task2: 85
+   }
+}
+const keys = Object.keys(student);
+const values = Object.values(student);
+console.log(values);
+
