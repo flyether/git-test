a9e3af7c3820f33cbee5132f92cb801fae4258ea
flyether <flyether@gmail.com>
2022-01-29 13:39:37 +0300

    fix: done

--- script.js
+++ script.js
@@ -1,15 +1,48 @@
-document.body.innerHTML = '<h1> Hello from JS</h1>'
-console.log('horse')
-
-const student = {
-   email: 'Valera',
-   name: 'tld@tut/by',
-   score:{
-      task1: 90,
-      task2: 85
+function getTranslate(event){
+const lang = event.currentTarget.dataset.language
+localStorage.setItem('lang', lang)
+languageSwitches.forEach(element => {element.classlist.remove('ru-language')})
+document.querySelectorAll('[data-i18]').forEach(el => {
+   el.textContent = i18Obj[lang][el.dataset.i18]
+   if (el.placeholder){
+      el.placeholder =  i18Obj[lang][el.dataset.i18]
+      el.textContent =''
    }
-}
-const keys = Object.keys(student);
-const values = Object.values(student);
-console.log(values);
+})
+   
+};
 
+languageSwitches.forEach((langSwitch) => {
+
+}
+
+// import i18Obj from `./translate` ; 
+// const ruLanguage = document.querySelector('.ru-language')
+// const enLanguage = document.querySelector('.en-language')
+
+// function getTranslate(lang) {
+//    const translate = document.querySelectorAll('[data.i18]') 
+// translate.forEach(ele=> {ele.textContent = i18Obj[lang][ele.dataset.i18] 
+// })
+// }
+
+// ruLanguage.addEventListener('click', getTranslate)
+// enLanguage.addEventListener('click', getTranslate)
+
+//оберните вызов getTranslate в функцию-коллбэк
+// ну и это всё равно не корректно, т.к. Вам тогда в getTranslate надо ещё определить по какой кнопке был клик
+
+//язык надо определить через event.target
+
+// const ruLanguage = document.querySelectorAll('.ru-language')
+// const enLanguage = document.querySelectorAll('.en-language')
+
+
+// function getTranslate(lang) {
+//    const translate = document.querySelectorAll('[data.i18]') 
+// translate.forEach(ele=> {ele.textContent = i18Obj[lang][ele.dataset.i18] 
+// })
+// }
+
+// ruLanguage.addEventListener('click', getTranslate)
+// enLanguage.addEventListener('click', getTranslate)
