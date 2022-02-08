aeaef566c76f8d864f2624f05c86a79cf21bffeb
flyether <flyether@gmail.com>
2022-02-08 15:07:37 +0300

    add: minsaze img

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 
-  const url = 'https://api.unsplash.com/search/photos?query=spring&per_page=30&orientation=landscape&client_id=SouHY7Uul-OxoMl3LL3c0NkxUtjIrKwf3tsGk1JaiVo'
-  
+  const url = "https://api.unsplash.com/photos/random?query=spring&client_id=SouHY7Uul-OxoMl3LL3c0NkxUtjIrKwf3tsGk1JaiVo"
+  const imgCard = document.querySelectorAll(".img-card")
   async function getData() {
-    const res = await fetch(url);
-    const data = await res.json();
-    console.log(data);
+    try{
+      const response = await fetch(url)
+      const data = await response.json()
+      console.log(data)
+      imgCard.scr = data.file
+    }catch(error){
+      console.log(error)
+    }       
   }
-  getData();
+  getData()
+
+
+
   
-  var block = document.getElementById('img-API');
+  var block = document.getElementById('img-API')
   function changeImg(){
     block.style.backgroundImage = "url('https://cs7062.vk.me/c540107/v540107359/2729/fYQlS_23QdA.jpg')";
 }
